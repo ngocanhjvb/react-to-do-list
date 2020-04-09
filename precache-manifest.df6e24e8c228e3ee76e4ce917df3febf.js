659ff3cda5861de9eb50329130139cb545c5e591
NgocAnh <ngoc.anh@jvb-corp.com>
2020-04-09 22:54:12 +0700

    Updates

---
+++ precache-manifest.df6e24e8c228e3ee76e4ce917df3febf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8962e87f6bebce824340d5fb7652aa19",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "a99a4afb7fade17e610f",
+    "url": "/react-to-do-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "917254cedfd53d8354d5",
+    "url": "/react-to-do-list/static/js/2.958fe1c8.chunk.js"
+  },
+  {
+    "revision": "26fc04af0b93e82b96d64c1a78861882",
+    "url": "/react-to-do-list/static/js/2.958fe1c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99a4afb7fade17e610f",
+    "url": "/react-to-do-list/static/js/main.254a936f.chunk.js"
+  },
+  {
+    "revision": "6b9d75263d19b05f2755",
+    "url": "/react-to-do-list/static/js/runtime-main.efe53d32.js"
+  }
+]);
